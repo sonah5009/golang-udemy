c5e6a749b2ea9977b9804e1c2296c26603f45a6b
sonah5009 <sonah500@gmail.com>
2024-10-29 13:41:54 +0900

    3-18: OO Approach vs Go Approach

--- cards/main.go
+++ cards/main.go
@@ -7,8 +7,7 @@ func main() {
 	cards = append(cards, "Six of Spades")
 
 	for i, card := range cards {
-		fmt.Println(card)
-		// if we don't use 'i' inside => can see "declared and not used"
+		fmt.Println(i, card)
 	}
 }
 
--- cards/section3.md
+++ cards/section3.md
@@ -2,3 +2,7 @@
 
 Array: Fixed len
 Slice: An array that can grow or shrink, every ele must be same type
+
+# 3-18. OO Approach vs Go Approach
+
+객체지향인 Python, Ruby 처럼 class 란 idea가 go 내부엔 없음
